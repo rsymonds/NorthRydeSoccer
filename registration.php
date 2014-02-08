dc76fa56b7e0d78036444077114da2a3d155a581
rsymonds <ricky.symonds@gmail.com>
2014-02-08 04:30:17 +0000

    Rego

--- registration.php
+++ registration.php
@@ -19,79 +19,193 @@ include "sitemenu.inc";?>
 				<div style="" class="general-index graphic">
 					<div class="figure-content"><!-- sandvox.GeneralIndex -->
 						<div class="article i1 o">
-							<h3 class="index-title"><span class="in">Registration</span></h3>
+							<h3 class="index-title"><span class="in">Fees for the 2014 Season</span></h3>
 							<div class="article-summary">
 								<div>
 									<p>
-									<ul>
-									<li><a href="documents/library/Fee Announcement- 2013.pdf" title="Registration Fees"><span class="in">2013 Registration Fees</span></a></li>
-									<li><a href="documents/library/Fee Announcement- 2013.pdf" title="Registration Fees"><span class="in">2013 Registration Form</span></a></li>
-									<li><a href="documents/library/Fee Announcement- 2013.pdf" title="Registration Fees"><span class="in">2013 Insurance Form</span></a></li>
-									<li><a href="documents/library/Fee Announcement- 2013.pdf" title="Registration Fees"><span class="in">FFA Registration Form</span></a></li>
-									</ul>
+									<div class="CSSTableGenerator" >
+                                        <table >
+                                            <tr>
+                                                <td>
+                                                    Age Group
+                                                </td>
+                                                <td >
+                                                    Registration Fee (Early Bird)
+                                                </td>
+                                                <td>
+                                                    Full Fee
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td >
+                                                    Under 6  - Boys and Girls
+                                                </td>
+                                                <td>
+                                                    $145.00
+                                                </td>
+                                                <td>
+                                                    $155.00
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td >
+                                                    Under 7 - Boys and Girls
+                                                </td>
+                                                <td>
+                                                    $145.00
+                                                </td>
+                                                <td>
+                                                    $155.00
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td >
+                                                    Under 8 - Boys and Girls
+                                                </td>
+                                                <td>
+                                                    $145.00
+                                                </td>
+                                                <td>
+                                                    $155.00
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td >
+                                                    Under 9 - Boys and Girls
+                                                </td>
+                                                <td>
+                                                    $160.00
+                                                </td>
+                                                <td>
+                                                    $70.00
+                                                </td>
+                                            </tr>
+                                           <tr>
+                                                <td >
+                                                    Under 10 - Boys and Girls
+                                                </td>
+                                                <td>
+                                                    $180.00
+                                                </td>
+                                                <td>
+                                                    $190.00
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td >
+                                                    Under 11 - Boys and Girls
+                                                </td>
+                                                <td>
+                                                    $180.00
+                                                </td>
+                                                <td>
+                                                    $190.00
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td >
+                                                    Under 12 - Boys and Girls
+                                                </td>
+                                                <td>
+                                                    $180.00
+                                                </td>
+                                                <td>
+                                                    $190.00
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td >
+                                                    Under 13 - Boys
+                                                </td>
+                                                <td>
+                                                    $190.00
+                                                </td>
+                                                <td>
+                                                    $200.00
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td >
+                                                    Under 14 - Boys
+                                                </td>
+                                                <td>
+                                                    $190.00
+                                                </td>
+                                                <td>
+                                                    $200.00
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td >
+                                                    Under 15 - Boys
+                                                </td>
+                                                <td>
+                                                    $195.00
+                                                </td>
+                                                <td>
+                                                    $205.00
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td >
+                                                    Under 16 - Boys
+                                                </td>
+                                                <td>
+                                                    $195.00
+                                                </td>
+                                                <td>
+                                                    $205.00
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td >
+                                                    Under 18 - Young Men
+                                                </td>
+                                                <td>
+                                                    $210.00
+                                                </td>
+                                                <td>
+                                                    $220.00
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td >
+                                                    All Age (Adult Mens)
+                                                </td>
+                                                <td>
+                                                    $325.00
+                                                </td>
+                                                <td>
+                                                    $335.00
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td >
+                                                    Over 35s - Mens
+                                                </td>
+                                                <td>
+                                                    $325.00
+                                                </td>
+                                                <td>
+                                                    $335.00
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td >
+                                                    Over 45s - Mens
+                                                </td>
+                                                <td>
+                                                    $325.00
+                                                </td>
+                                                <td>
+                                                    $335.00
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </div>
 									</p>
 								</div>
 							</div>
-						</div>
-						<div class="article i2 e">
-							<h3 class="index-title"><span class="in">Substitution Sheets</span></h3>
-							<div class="article-summary">
-								<div>
-									<p>
-									<ul>
-									<li><a href="documents/library/substitution_sheet_4_a_side.pdf" title="Four-a-Side"><span class="in">Four-a-Side</span></a></li>
-									<li><a href="documents/library/substitution_sheet_5_a_side.pdf" title="Five-a-Side"><span class="in">Five-a-Side</span></a></li>
-									<li><a href="documents/library/substitution_sheet_7_a_side.pdf" title="Seven-a-Side"><span class="in">Seven-a-Side</span></a></li>
-									<li><a href="documents/library/substitution_sheet_9_a_side.pdf" title="Nine-a-Side"><span class="in">Nine-a-Side</span></a></li>
-									<li><a href="documents/library/substitution_sheet_11_a_side.pdf" title="Eleven-a-Side"><span class="in">Eleven-a-Side</span></a></li>
-									</ul>
-									</p>
-								</div>
-							</div>
-						</div>
-						<div class="article i3 o">
-							<h3 class="index-title"><span class="in">Minis Ideas & Info</span></h3>
-							<div class="article-summary">
-								<div>
-									<p>
-									<ul>
-									<li><a href="documents/library/award_certificate_ideas.doc" title="Awards & Certificates"><span class="in">Awards & Certificates</span></a></li>
-									<li><a href="documents/library/award_certificate_template.ppt" title="Certificate Template"><span class="in">Certificate Template</span></a></li>
-									<li><a href="documents/library/award_player_of_the_week.pdf" title="Player Of The Week"><span class="in">Player Of The Week</span></a></li>
-									<li><a href="documents/library/small_sided_games_quick_guide.doc" title="Small Sided Game Guide"><span class="in">Small Sided Game Guide</span></a></li>
-									<li><a href="documents/library/small_sided_games_helpful_hints.pdf" title="Small Sided Game Hints"><span class="in">Small Sided Game Hints</span></a></li>
-									</ul>
-									</p>
-								</div>
-							</div>
-						</div>
-						<div class="article i2 e">
-							<h3 class="index-title"><span class="in">Info for Officials</span></h3>
-							<div class="article-summary">
-								<div>
-									<p>
-									<ul>
-									<li><a href="documents/library/manager_duties_minis.doc" title="Managers Duties - Minis"><span class="in">Managers Duties - Minis</span></a></li>
-									<li><a href="documents/library/coaches_and_officials_guidelines.doc" title="General Guidelines"><span class="in">General Guidelines</span></a></li>
-									<li><a href="documents/library/coaches_and_managers_season_preparation.pdf" title="Season Preparation"><span class="in">Season Preparation</span></a></li>
-									</ul>
-									</p>
-								</div>
-							</div>
-						</div>
-						<div class="article i2 o last-item">
-							<h3 class="index-title"><span class="in">General Documents</span></h3>
-							<div class="article-summary">
-								<div>
-									<p>
-									<ul>
-									<li><a href="documents/library/nrsc_declaration.pdf" title="NRSC Declaration"><span class="in">NRSC Declaration</span></a></li>
-									<li><a href="documents/library/working_with_children_declaration.pdf" title="WWC Declaration"><span class="in">WWC Declaration</span></a></li>
-									<li><a href="documents/library/code_of_conduct.pdf" title="Code of Conduct"><span class="in">Code of Conduct</span></a></li>
-									<li><a href="documents/library/id_card_instructions.pdf" title="Id Card Instructions"><span class="in">Id Card Instructions</span></a></li>
-									</ul>
-									</p>
-								</div>
 						</div>
 						<div class="general-index-bottom"></div>
 					</div>
